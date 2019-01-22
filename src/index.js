89fda5e63767fa45425cc90ea383cad8ff2922e5
Valentyna Tuparieva <valentyna_tuparieva@epam.com>
2019-01-22 14:18:32 +0200

    changed file

--- src/index.js
+++ src/index.js
@@ -1,39 +1,47 @@
 const R = require('ramda');
 
-let arr = [-1,7,9];
-
 //First task
-Array.prototype.myMap = function(callback) {
-    arr = [];
-    for (var i = 0; i < this.length; i++)
-        arr.push(callback(this[i], i, this));
+Array.prototype.myMap = function(callback, thisArg) {
+    let arr = [];
+    for (let i = 0; i < this.length; i++) {
+        if (this.hasOwnProperty(i)) {
+            arr.push(callback.call(thisArg, this[i], i ,this));
+        }
+    }
     return arr;
 };
 
+
 //Second task
 Array.prototype.myReduce = function(callback, initialVal) {
-    var accumulator = (initialVal === undefined) ? undefined : initialVal;
-    for (var i = 0; i < this.length; i++) {
-        if (accumulator !== undefined)
+    let accumulator = (initialVal === undefined) ? undefined : initialVal;
+    if (accumulator == undefined) {
+        accumulator = 0;
+    }
+    for (let i = 0; i < this.length; i++) {
+        if (this.hasOwnProperty(i)) {
             accumulator = callback.call(undefined, accumulator, this[i], i, this);
-        else
-            accumulator = this[i];
+        }
     }
     return accumulator;
 };
 
 //Third task
-function multiply(x) {
-    return function(y) {
-        return (typeof y !== 'undefined') ?  x*y :  x;
+function multiply(a) {
+    let currentMul = a;
+
+    function f(b) {
+      currentMul *= b;
+      return f;
     }
+
+    f.toString = function() {
+      return currentMul;
+    };
+
+    f.value = f;
+    return f;
 }
-
-
-arr.myMap((item)=> item*2);
-let myReduceResult = arr.myReduce((sum, current) => {return sum + current});
-
-multiply(4)(4);
 
 //Fourth task - Multiply(x)(y) with Ramda
 R.multiply(7)(10);
